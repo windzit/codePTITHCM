ef19fb73798deb4aa4f012830d361cf64b818fc8
Wind Zit <chauphong1604@gmail.com>
2026-02-07 23:07:01 +0700

    Toi uu ICPC0113.py

--- ICPC0113.py
+++ ICPC0113.py
@@ -1,20 +1,17 @@
 #EMIRP NUMBER
-
-MAXN = 10**6 + 1
-is_prime = [True] * MAXN
-is_prime[0] = is_prime[1] = False
-
-
-for i in range(2, MAXN): #create list prime 2->10^6
-    if is_prime[i]:
-        for j in range(i*2, MAXN, i):
-            is_prime[j] = False
-
 for _ in range(int(input())):
     N = int(input())
+    MAXN = N + 1
+    is_prime = [True] * MAXN
+    is_prime[0] = is_prime[1] = False
+
+
+    for i in range(2, MAXN):
+        if is_prime[i]:
+            for j in range(i*2, MAXN, i):
+                is_prime[j] = False
     
     check = set()
-    result = []
     
     for i in range(2, N+1):
         if not is_prime[i] or i in check:
@@ -27,7 +24,6 @@ for _ in range(int(input())):
         
         if is_prime[reverse]:
             check.update([i, reverse])
-            result.extend([i, reverse])
-            
-    print(*result)
+            print(i, reverse, end=" ")
+    print()
         
